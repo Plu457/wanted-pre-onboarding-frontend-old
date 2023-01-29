8f750552f92344b880857d34fa6dc37451c83233
plu457 <ekgus5095@gmail.com>
2023-01-29 14:21:05 +0900

    Docs: 프로젝트 세팅 및 README.md 파일 내용 추가

---
+++ src/Router.js
@@ -0,0 +1,15 @@
+import Home from "Home";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
